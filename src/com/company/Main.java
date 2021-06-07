ef5c199012b882b1e44fa6efe31025a39fa5f44d
Fateme <fatemetalebi91@gmail.com>
2021-06-07 14:41:25 +0430

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,13 @@ import com.company.Classes.NoFly;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("***Strategy Design Patern***");
+        System.out.println("************Start***********");
         Bird eagle=new Eagle();
         eagle.performFly();
         eagle.setFlyBehavior(new NoFly());
         eagle.performFly();
+        System.out.println("************End*************");
     }
 }
